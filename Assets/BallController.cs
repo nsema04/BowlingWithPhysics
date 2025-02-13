1e6c84a6fa2f5942e6565424f2f7d41e6e46f72f
Noah Semashkewich <noahsemashkewich@Mac.lan>
2025-02-13 00:32:53 -0800

    Added force on sphere
    The sphere launches when the user clicks space.

---
+++ Assets/BallController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour {
+
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager InputManager;
+
+    private Rigidbody ballRB;
+
+    void Start()
+    {
+        //Grabbing a reference to RigidBody
+        ballRB = GetComponent<Rigidbody>();
+
+        // Add a listener to the OnSpacePressed event
+        //When the space key is pressed the
+        //LaunchBall method will be called
+        InputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall()
+    {
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+}
