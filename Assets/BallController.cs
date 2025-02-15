ddb5a0cdbe8e4b86457fb640d71d45b5bb3cb770
Noah Semashkewich <noahsemashkewich@Mac.lan>
2025-02-15 15:38:49 -0800

    Added ball moving with the player
    BallAnchor keeps the ball moving with the player before the ball is launched.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,20 +4,28 @@ using UnityEngine.Events;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
     [SerializeField] private InputManager inputManager;
+
+
     private bool isBallLaunched;
     private Rigidbody ballRB;
-
+    
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall()
     {
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
