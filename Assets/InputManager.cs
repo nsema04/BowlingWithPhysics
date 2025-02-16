dcca3ca34818a53d898a34fa5cbdca54eea4a6c5
Noah Semashkewich <noahsemashkewich@Mac.lan>
2025-02-15 18:17:41 -0800

    Added Reset Functionality
    Added functionality to reset and spawn new pins by pressing R.

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -5,6 +5,7 @@ public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
+    public UnityEvent OnResetPressed = new UnityEvent();
 
     private void Update()
     {
@@ -23,5 +24,10 @@ public class InputManager : MonoBehaviour
             input += Vector2.right;
         }
         OnMove?.Invoke(input);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OnResetPressed?.Invoke();
+        }
     }
 }
